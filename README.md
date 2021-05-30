1d8e6138a7e752e73c696d4b2df86f26e2c4ae7e
xolar1989 <awglm12345>
2021-05-30 12:13:20 +0200

    update readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# Braessa Paradox
+Simple Application to show Braessa Paradox using concurrency (notifyall , wait ,Runnable )
+
+### Building
+
+In order to build project generating jar with sources use :
+
+```sh
+mvn -Prelease clean package
+```
+### Running
+```sh
+java -jar target/atos-1.0-SNAPSHOT.jar
+```
+
+### Author
+
+Karol Wiśniewski ,2021
+
+
+
--- src/main/java/rozproszone/lab/Main.java
+++ src/main/java/rozproszone/lab/Main.java
@@ -7,12 +7,26 @@ import rozproszone.lab.traffic.service.TrafficService;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        RoadRepository roadRepository = new RoadRepository(5) ;
+
+        System.out.println("Time with 4 roads :");
+        RoadRepository roadRepository = new RoadRepository(4) ;
         CarRepository carRepository = new CarRepository(roadRepository) ;
 
 
         TrafficService trafficService = new TrafficService(carRepository,roadRepository) ;
 
         trafficService.startTraffic();
+
+        System.out.println("Time with 5 roads :");
+
+        RoadRepository roadRepository2 = new RoadRepository(5) ;
+        CarRepository carRepository2 = new CarRepository(roadRepository) ;
+
+
+        TrafficService trafficService2 = new TrafficService(carRepository,roadRepository) ;
+
+        trafficService2.startTraffic();
+
+
     }
 }
